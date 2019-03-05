10e9470abae5726e9b6d6d95dc86d9da2a8e12f8
dark <122712246@qq.com>
2019-03-05 13:24:54 +0800

    reversList

---
+++ Algorithm/link_list/reverse-linked-list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+class ListNode:
+    def __init__(self, x):
+        self.var = x
+        self.next = None
+
+
+class Solution:
+    def reverseList(self, head:ListNode) ->ListNode:
+        pre = None
+        cur = head
+        while cur:
+            nextnode = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nextnode
+        return pre
+
+
+
