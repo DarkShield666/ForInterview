18c2b0fb4fe896e490d35b6c62c5d0f32a1dd9b4
DarkShield666 <daigj>
2019-12-17 19:51:29 +0800

    binaryTree

--- Algorithm/tree/inorderTraversal.py
+++ Algorithm/tree/inorderTraversal.py
@@ -22,6 +22,126 @@ class TreeNode:
         self.right = None
 
 
-class Solution:
-    def inorderTraversal(self, root: TreeNode) -> List[int]:
-        pass
+# Definition for a tree
+class Tree:
+    def __init__(self):
+        self.root = None
+        self.lis = []
+
+        self.preRsult = []
+        self.inRsult = []
+        self.postResult = []
+
+    def addNode(self, val):
+        node = TreeNode(val)                        # 实例化树节点
+        if self.root == None:                       # 判断根节点是否为None，若为None，将根节点地址值添加到lis中
+            self.root = node
+            self.lis.append(self.root)
+        else:
+            rootNode = self.lis[0]                  # 将第一个元素设置为根节点
+            if rootNode.left == None:
+                rootNode.left = node
+                self.lis.append(rootNode.left)
+            elif rootNode.right == None:
+                rootNode.right = node
+                self.lis.append(rootNode.right)
+                self.lis.pop(0)
+
+
+    def preOrderTraversal(self, root):
+        if root == None:
+            return
+        self.preRsult.append(root.val)
+        self.preOrderTraversal(root.left)
+        self.preOrderTraversal(root.right)
+
+    def preOrderStack(self, root):
+        if root == None:
+            return
+        stack = []
+        result = []
+        node = root
+        while node or stack:
+            while node:
+                result.append(node.val)
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            node = node.right
+        print(result)
+
+
+    def inOrderTraversal(self, root):
+        if root == None:
+            return
+        self.inOrderTraversal(root.left)
+        # print(root.val)
+        self.inRsult.append(root.val)
+        self.inOrderTraversal(root.right)
+
+    def inOrderStack(self, root):
+        if root == None:
+            return
+        stack = []
+        result = []
+        node = root
+        while node or stack:
+            while node:
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            result.append(node.val)
+            node = node.right
+        print(result)
+
+
+    def postOrderTraversal(self, root):
+        if root == None:
+            return
+        self.postOrderTraversal(root.left)
+        self.postOrderTraversal(root.right)
+        self.postResult.append(root.val)
+
+    def postOrderStack(self, root):
+        if root == None:
+            return
+        stack = []
+        seq = []
+        result = []
+        node = root
+        while node or stack:
+            while node:
+                seq.append(node.val)
+                stack.append(node)
+                node = node.right
+            node = stack.pop()
+            node = node.left
+        while seq:
+            result.append(seq.pop())
+        print(result)
+
+
+# class Solution:
+    # def inorderTraversal(self, root: TreeNode) -> List[int]:
+    #     pass
+
+
+if __name__ == '__main__':
+    T = Tree()
+    for i in range(1,11):
+        T.addNode(i)
+
+    T.preOrderTraversal(T.root)
+    print(T.preRsult)
+    T.preOrderStack(T.root)
+
+    T.inOrderTraversal(T.root)
+    print(T.inRsult)
+    T.inOrderStack(T.root)
+
+    T.postOrderTraversal(T.root)
+    print(T.postResult)
+    T.postOrderStack(T.root)
+
+
+
